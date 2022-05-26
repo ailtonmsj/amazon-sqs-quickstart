08ce29e7562abb20f797b29829078726104ab1cb
ailton.msj <ailton.msj@gmail.com>
2022-05-26 10:34:27 -0300

    Comentado tudo, so para testar

--- src/main/java/br/com/amsj/sqs/consumer/QuarksShieldSyncResource.java
+++ src/main/java/br/com/amsj/sqs/consumer/QuarksShieldSyncResource.java
@@ -1,49 +1,70 @@
 package br.com.amsj.sqs.consumer;
 
 import java.util.List;
-
-import javax.inject.Inject;
+import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
 import io.quarkus.scheduler.Scheduled;
+import io.smallrye.mutiny.Uni;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.core.SdkSystemSetting;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 public class QuarksShieldSyncResource {
 
     private static final Logger LOGGER = Logger.getLogger(QuarksShieldSyncResource.class);
 
-    @Inject
-    SqsClient sqs;
+//    @Inject
+//    SqsClient sqs;
 
     @ConfigProperty(name = "queue.url")
     String queueUrl;
 
     @Scheduled(every = "10s")
     public void receive() {
+    	
+    	System.setProperty(
+    			SdkSystemSetting.SYNC_HTTP_SERVICE_IMPL.property(),
+    			"software.amazon.awssdk.http.apache.ApacheSdkHttpService");
+    	
+    	SqsClient sqs = SqsClient.builder()
+    			.credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+    			.build();
+    	
+    	LOGGER.info("FILA SNS" + queueUrl);
+    	
+    	
         List<Message> messages = sqs.receiveMessage (m -> m.maxNumberOfMessages(10).queueUrl(queueUrl)).messages();
         
         LOGGER.info(">> MESSAGE QTIDE: " + messages.size());
         
-        for(Message message : messages) {
-        	
-        	LOGGER.info(">> MESSAGE BODY: " + message.body());
-        	
-        	LOGGER.info(">> MESSAGE PREPARANDO PARA SER DELETADA...");
-        	
-        	DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
-                    .queueUrl(queueUrl)
-                    .receiptHandle(message.receiptHandle())
-                    .build();
-        	
-        	LOGGER.info(">> MESSAGE A SER DELETADA: " + message.receiptHandle());
-        	
-        	sqs.deleteMessage(deleteMessageRequest);
-        	
-        	LOGGER.info(">> MESSAGE DELETADA COM SUCESSO!!!");
-        }
+//        for(Message message : messages) {
+//        	
+//        	//LOGGER.info(">> MESSAGE BODY: " + message.body());
+//        	
+//        	LOGGER.info(">> MESSAGE PREPARANDO PARA SER DELETADA...");
+//        	
+//        	DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder()
+//                    .queueUrl(queueUrl)
+//                    .receiptHandle(message.receiptHandle())
+//                    .build();
+//        	
+//        	LOGGER.info(">> MESSAGE A SER DELETADA: " + message.receiptHandle());
+//        	
+//        	sqs.deleteMessage(deleteMessageRequest);
+//        	
+//        	LOGGER.info(">> MESSAGE DELETADA COM SUCESSO!!!");
+//        }
+        
+        LOGGER.info(">> FIM");
     }
+
 }
